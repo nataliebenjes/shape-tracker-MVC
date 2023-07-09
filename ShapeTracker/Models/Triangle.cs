6eb4a9a9505a4006682b66d0753afe076ff1867f
Daniel Kiss <danielkiss@MacBook-Pro.lan>
2023-07-09 16:11:25 -0700

    add side 1 get functionality and passing test

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -2,10 +2,17 @@ namespace ShapeTracker.Models
 {
     public class Triangle
     {
-        // Code for Triangle business logic will go here.
-        public Triangle()
+        private int _side1;
+
+        // this is the getter and setter
+        public int Side1
         {
-            //
+            get { return _side1; }
+        }
+        // Code for Triangle business logic will go here.
+        public Triangle(int length1)
+        {
+            _side1 = length1;
         }
     }
 }
