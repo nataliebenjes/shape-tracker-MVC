ce81db459e9f933509d4731b9c78b9c4e14ebb19
Natalie benjes <natalie.benjes@gmail.com>
2023-07-17 15:34:28 -0700

    Add model and view for form.
    Co-authored-by: Daniel Kiss <flowfast47@gmail.com>

--- ShapeTracker/Controllers/HomeController.cs
+++ ShapeTracker/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ShapeTracker.Controllers
 // namespace ShapeController
 {
-    public class ShapeController: Controller
+    public class ShapeController : Controller
     {
         // get a form to get inputs, then on submit take user to next view which will show the type of triangle
 
@@ -12,20 +12,24 @@ namespace ShapeTracker.Controllers
         // [Get("/")]
 
         // ActivePage 
-        public ActionResult BasePage()
+        public ActionResult BasePage(int side1, int side2, int side3)
         {
             // should be a form to gather the sides
-            Triangle newTriange = new Triangle(1,2,3);
+            Triangle newTriange = new Triangle(side1, side2, side3);
             return View(newTriange);
         }
+        [Route("/form")]
+        public ActionResult Form() { return View(); }
 
-        [Route("/yo")]
-        // [Get("/")]
-
-        // ActivePage 
-        public string hi()
+        [Route("/")]
+        public string Hi()
         {
-            return "hi";
+            return "sample home";
+        }
+        [Route("/HeyYou")]
+        public string Hey()
+        {
+            return "Hey You";
         }
     }
 }
